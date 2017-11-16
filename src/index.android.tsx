abfa46e22082ffee975b1eaccda9ff2cbc4b6fec
chauncey <realchauncey@gmail.com>
2017-11-17 00:03:43 +0800

    init

---
+++ src/index.android.tsx
@@ -0,0 +1,52 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+
+import React from 'react'
+
+export interface Props { }
+export interface State { }
+
+const styles: any = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF'
+    },
+    welcome: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    instructions: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
+})
+
+export default class AwesomeProject extends React.Component<Props, State> {
+    render() {
+        return (
+            <View style={styles.container}>
+              <Text style={styles.welcome}>
+                Welcome to React Native! (in TypeScript)
+              </Text>
+              <Text style={styles.instructions}>
+                To get started, edit index.android.js
+              </Text>
+              <Text style={styles.instructions}>
+                Press Double click R to reload,{'\n'}
+                ? or shake for dev menu
+              </Text>
+            </View>
+        )
+    }
+}
+
+AppRegistry.registerComponent('AwesomeProject', () => AwesomeProject)
